1eb2be1f90128dfa6ff33d531ce79172b3ef0cc3
Hameed Damee <dameeimpact@gmail.com>
2019-10-02 13:56:46 +0100

    bug fixes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb://hameedDamee:hameedDamee@cluster0-ttinl.mongodb.net/bizjot"
+      "mongodb://hameedDamee:hameedDamee@cluster0-ttinl.mongodb.net/BizJot?retryWrites=true&w=majority"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/bizjot-dev" };
