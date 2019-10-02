fdbd2e278bf0b7aa71a917e7399da10272e15c5e
Hameed Damee <dameeimpact@gmail.com>
2019-10-02 15:04:14 +0100

    app finally working

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI:
-      "mongodb+srv://hameedDamee:hameedDamee@cluster0-ttinl.mongodb.net/bizjot?retryWrites=true&w=majority"
+    mongoURI: "CHANGE TO ATLAS URL"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/bizjot-dev" };
