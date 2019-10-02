937bb5e2e2f5c96f20e2337e3db57e5e0576f4b2
Hameed Damee <dameeimpact@gmail.com>
2019-10-02 11:37:12 +0100

    mongodb error fixes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI: "REPLACE WITH PRODUCTION MONGODB CONNECTION STRINGS"
+    mongoURI:
+      "mongodb+srv://hameedDamee:hameedDamee@cluster0-ttinl.mongodb.net/test?retryWrites=true&w=majority"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/bizjot-dev" };
