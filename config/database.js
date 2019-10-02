31c3a4dd825079da70642b68bc5127eff01b3969
Hameed Damee <dameeimpact@gmail.com>
2019-10-02 11:25:14 +0100

    All done

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI:
-      "mongodb+srv://hameedDamee:hameedDamee@cluster0-ttinl.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "REPLACE WITH PRODUCTION MONGODB CONNECTION STRINGS"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/bizjot-dev" };
